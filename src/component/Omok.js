ebfeec308b58274295d0ea4d3e0208e04deefb24
jooha <dlwngk14@naver.com>
2020-04-23 19:10:05 +0900

    오목게임

--- src/component/Omok.js
+++ src/component/Omok.js
@@ -18,11 +18,14 @@ const useStyles = makeStyles (() => ({
         "display": "inline-block",
         "margin": "0 2px"
     },
-    "plusmark": { "cursor": "pointer", "margin": "0 4px" },
+    "plusmark": {
+        "margin": "0 4px" },
     "div_hei": {"height": "29px"}
 }));
 
-const omok_frame = function make_frame() {
+let num = 19, count = 0, done = false;
+
+function make_frame() {
     const arr = Array();
     for (let i=0; i<num+1; i++) {
         const chd_addr = Array();
@@ -48,19 +51,107 @@ const omok_frame = function make_frame() {
     return arr;
 }
 
+const omok_frame = make_frame();
 
-let num = 19, count = 0, now = '0';
+function five_check(li, row, col) {
+    let cnt = 1;
+    let row_m = row, row_p = row;
+    let col_m = col, col_p = col;
+    const stone = li[row][col];
+
+    // '\' 모양 체크
+    for (let i=0; i<4; i++) {
+        --col_m;
+        --row_m;
+        const chk_stone = li[row_m][col_m];
+        if (col_m < 1 || row_m < 1 || chk_stone !== stone) break;
+        ++cnt;
+    }
+    for (let i=0; i<4; i++) {
+        ++col_p;
+        ++row_p;
+        const chk_stone = li[row_p][col_p];
+        if (col_p > num-1 || row_p > num-1 || chk_stone !== stone) break;
+        ++cnt;
+    }
+    if (cnt === 5) return true;
+    ////////////////////
+    // '/' 모양 체크
+    cnt = 1;
+    row_m = row_p = row;
+    col_m = col_p = col;
+    for (let i=0; i<4; i++) {
+        --row_m;
+        ++col_p;
+        const chk_stone = li[row_m][col_p];
+        if (row_m < 1 || col_p > num-1 || chk_stone !== stone) break;
+        ++cnt;
+    }
+    for (let i=0; i<4; i++) {
+        ++row_p;
+        --col_m;
+        const chk_stone = li[row_p][col_m];
+        if (row_p > num-1 || col_m < 1 || chk_stone !== stone) break;
+        ++cnt;
+    }
+    if (cnt === 5) return true;
+    //////////////////////
+    // 'ㅡ' 모양 체크
+    cnt = 1;
+    col_m = col_p = col;
+    for (let i=0;i<4;i++) {
+        --col_m;
+        const chk_stone = li[row][col_m];
+        if (col_m < 1 || stone !== chk_stone) break;
+        ++cnt;
+    }
+
+    for (let i=0;i<4;i++) {
+        ++col_p;
+        const chk_stone = li[row][col_p];
+        if (col_p > num-1 || stone !== chk_stone) break;
+        ++cnt;
+    }
+    if (cnt === 5) return true;
+    //////////////////////
+    // 'ㅣ' 모양 체크
+    cnt = 1;
+    row_m = row_p = row;
+    for (let i=0; i<4;i++) {
+        --row_m;
+        const chk_stone = li[row_m][col];
+        if (row_m < 1 || chk_stone !== stone) break;
+        ++cnt;
+    }
+    for (let i=0;i<4;i++) {
+        ++row_p;
+        const chk_stone = li[row_p][col];
+        if (row_p > num-1 || chk_stone !== stone) break;
+        ++cnt;   
+    }
+    return cnt === 5 ? true : false;
+    //////////////////////
+}
+
 
 export default function Omok() {
     const classes = useStyles();
     const [frame, setFrame] = useState(omok_frame);
 
     const set_stone = (s, row, col) => {
-        if (s !== '+') return;
+        if (s !== '+' || done) return;
         const tmp = [...frame];
         tmp[row][col] = count%2 === 1 ? '1' : '0';
         ++count;
         setFrame(tmp);
+        if (five_check(tmp, row, col)) {
+            done = true;
+            const st = tmp[row][col] === '1' ? "흑돌" : "백돌";
+            console.log(count);
+            setTimeout(() => {
+                alert(st + " 승리 !");
+            }, 400);
+        }
         if ((num-1)*(num-1) === count) {
             setTimeout(() => {
                 alert("게임이 끝났습니다.");
